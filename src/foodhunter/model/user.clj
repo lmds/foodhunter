f5c594b607c115cdf45c58eb4da54fe7d4910c41
Yogthos <yogthos@gmail.com>
2011-09-14 21:34:54 -0400

    added password hashing

--- src/foodhunter/model/user.clj
+++ src/foodhunter/model/user.clj
@@ -6,26 +6,30 @@
   (atom 
     {"john.doe@gmail.com"
      {:info 
-      {:firstname "John", :lastname "Doe", :password "foo"}
+      {:firstname "John", :lastname "Doe", :password "$2a$10$8XYCZ9mUfbjScZqBcGEUkOCqljif/Im8Uoz7budcSgN1PVQcwSmzu"}
       
       :restaurants
       {"Salad King"    {:rating 5 :style "South Asian"}
        "Matagali"      {:rating 4 :style "Indian"}
        "Sushi & Bento" {:rating 5 :style "Japanese"}}}}))
 
-(defn validate-user [username password]    
-  (= password (:password (:info (get @*users* username)))))
+
+(defn validate-user [username password]      
+  (noir.util.crypt/compare password (:password (:info (get @*users* username)))))
+
 
 (defn add-user [firstname lastname email password]  
   (swap! *users*
          (fn [users]
-           (assoc users email {:info {:firstname firstname, :lastname lastname, :password password}}))))
+           (assoc users email {:info {:firstname firstname, :lastname lastname, :password (noir.util.crypt/encrypt password)}}))))
+
 
 (defn get-profile [userid]
   (:info (get @*users* userid)))
 
 (defn get-restaurants [user]  
   (:restaurants (get @*users* user)))
+
 
 (defn add-restuarant [user restaurant rating style]  
   (swap! *users*
@@ -34,12 +38,14 @@
                       (fn [restaurants] 
                         (assoc restaurants restaurant {:rating rating :style style}))))))
 
+
 (defn remove-restuarant [user restaurant]
   (swap! *users*
          (fn [users]
            (update-in users [user :restaurants]
                       (fn [restaurants]
                         (dissoc restaurants restaurant))))))
+
 
 (defn update-rating [user name rating]  
   (swap! *users*
